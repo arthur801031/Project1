67c92e99b7fae5123a6b098ad083b2168f576cd8
i-chun <arthur801031@gmail.com>
2014-02-08 11:08:06 -0600

    Finish demo app

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Project1::Application.routes.draw do
 
   resources :musicians
 
+  root to: "musics#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
